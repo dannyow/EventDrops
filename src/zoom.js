319d1dbd8513a523ef11ca781e10f24288a959db
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-11-13 18:29:26 +0100

    Add start, middle and end zoom handlers

--- src/zoom.js
+++ src/zoom.js
@@ -1,10 +1,24 @@
 export default (d3, svg, config, xScale, draw, getEvent) => {
+    const {
+        zoom: {
+            onZoomStart,
+            onZoom,
+            onZoomEnd,
+        },
+    } = config;
+
     const zoom = d3.zoom();
 
-    zoom.on('zoom', () => {
+    zoom.on('zoom.start', onZoomStart).on('zoom.end', onZoomEnd);
+
+    zoom.on('zoom', (...args) => {
         const newScale = getEvent().transform.rescaleX(xScale);
         // @FIXME: remove d3.selectAll to use current selection
         d3.selectAll('svg').call(draw(config, newScale));
+
+        if (onZoom) {
+            onZoom(args);
+        }
     });
 
     return zoom;

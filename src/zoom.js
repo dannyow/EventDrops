c6e4d8040b67cd01e7f25f1f948294b4ef29de07
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-11-13 18:19:48 +0100

    Expose scale and displayedData methods

--- src/zoom.js
+++ src/zoom.js
@@ -4,7 +4,7 @@ export default (d3, svg, config, xScale, draw, getEvent) => {
     zoom.on('zoom', () => {
         const newScale = getEvent().transform.rescaleX(xScale);
         // @FIXME: remove d3.selectAll to use current selection
-        d3.selectAll('svg').call(draw(d3, config, newScale));
+        d3.selectAll('svg').call(draw(config, newScale));
     });
 
     return zoom;
